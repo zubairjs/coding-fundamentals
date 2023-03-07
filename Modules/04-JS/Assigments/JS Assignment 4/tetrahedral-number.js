184c632f3d21d99bb091be8cb868bd4dc5149694
zubairjs <115981303+zubairjs@users.noreply.github.com>
2023-03-07 18:46:51 +0800

    Add files via upload

---
+++ Modules/04-JS/Assigments/JS Assignment 4/tetrahedral-number.js
@@ -0,0 +1,14 @@
+// Create a function that takes an integer n and returns the nth tetrahedral number.
+// Examples:
+// tetra(2) ➞ 4
+// tetra(5) ➞ 35
+// tetra(6) ➞ 56
+
+function tetra(n) {
+	// your code
+	return (n * (n + 1) * (n + 2)) / 6;
+}
+
+console.log(tetra(2)); // 4
+console.log(tetra(5)); // 35
+console.log(tetra(6)); // 56
